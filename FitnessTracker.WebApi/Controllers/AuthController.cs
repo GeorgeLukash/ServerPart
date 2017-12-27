42f9ec7e4a1fcf60e7b572e8af8597b92e472fc9
Лукащук Юрій <guitar_lukas@outlook.com>
2017-12-27 18:44:52 +0200

    new commit

--- FitnessTracker.WebApi/Controllers/AuthController.cs
+++ FitnessTracker.WebApi/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using FitnessTracker.DataModel;
 using FitnessTracker.Operations.Abstraction;
+using FitnessTracker.WebApi.Filters;
 using FitnessTracker.WebApi.Providers.Abstraction;
 using System;
 using System.Web.Http;
@@ -48,6 +49,22 @@ namespace FitnessTracker.WebApi.Controllers
             catch (Exception)
             {
                 return BadRequest();
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [AuthorizeIfTokenValid]
+        [Route("get_me")]
+        public IHttpActionResult GetMyAuthInfo()
+        {
+            try
+            {
+                return Ok(_authOperations.GetMe(_currentUserProvider.CurrentUserId));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
                 throw;
             }
         }
